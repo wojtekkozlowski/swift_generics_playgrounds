9a70af169dd3db57aa8dd926449cc1601dd2c465
Wojtek Kozlowski <maniacus.gd@gmail.com>
2016-08-22 21:20:26 +1000

    sync

---
+++ _drafts.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+import UIKit
+
+//class Base { }
+//class Derived : Base { }
+//
+//var arrayOfDerived: [Derived] = [Derived(), Derived(), Derived()]
+//var arrayOfBase: [Base] = arrayOfDerived
+//arrayOfBase[0] = Base()
